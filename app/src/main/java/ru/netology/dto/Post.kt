1252b5e94ee0704ea218cec846016997dabc8936
Kirill Salov <steam-moi-v1.3@yandex.ru>
2022-11-17 16:41:28 +0400

    add listeners

---
+++ app/src/main/java/ru/netology/dto/Post.kt
@@ -0,0 +1,36 @@
+package ru.netology.dto
+
+import android.widget.ImageView
+
+data class Post(
+    val id: Int,
+    val author: String,
+    val published: String,
+    val content: String,
+    var likesCount: Int,
+    var sharesCount: Int,
+    var viewsCount: Int,
+    var likedByMe: Boolean
+) {
+
+    fun setCount(count: Int): String {
+        var stringCount = count.toString()
+        var countArray = stringCount.toCharArray()
+        when {
+            count in 0..999 -> return "$count"
+            count in 1000..9999 -> {
+                return if (countArray[1].equals('0')) "${countArray[0]}K" else "${countArray[0]}.${countArray[1]}K"
+            }
+            count in 10000..99_999 -> return "${countArray[0]}${countArray[1]}K"
+            count in 100_000..999_999 -> return "${countArray[0]}${countArray[1]}${countArray[2]}K"
+            count in 1_000_000..9_000_000 -> {
+                return if (countArray[1].equals('0')) "${countArray[0]}M" else "${countArray[0]}.${countArray[1]}M"
+            }
+            count in 10_000_000..99_000_000 -> {
+                return if (countArray[2].equals('0')) "${countArray[0]}${countArray[1]}M" else "${countArray[0]}${countArray[1]}.${countArray[2]}M"
+            }
+            else -> return "${countArray[0]}${countArray[1]}${countArray[2]}.${countArray[3]}M"
+        }
+        return "error"
+    }
+}
